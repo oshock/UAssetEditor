ba1798aa2881fc7d3c5c615dd247b2da4954d026
Owen <47339578+oshock@users.noreply.github.com>
2024-11-23 21:33:48 -0600

    wtf

---
+++ UAssetEditor/Unreal/Packages/FPakEntry.cs
@@ -0,0 +1,18 @@
+﻿using UAssetEditor.Unreal.Containers;
+
+namespace UAssetEditor.Unreal.Packages;
+
+public class FPakEntry : UnrealFileEntry
+{
+    public FPakEntry(string path, ContainerFile container) : base(path, container)
+    {
+    }
+
+    public override bool IsEncrypted { get; }
+    public override string CompressionMethod { get; }
+    
+    public override byte[] Read()
+    {
+        throw new NotImplementedException();
+    }
+}
