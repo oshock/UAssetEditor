acf291e008c7c7dc6a7a8fda59a558c642dee165
Dave <renegadesglitches@gmail.com>
2025-06-19 17:23:45 +0400

    FBox2D.cs FColor.cs FIntVector.cs FQuat.cs
    FTransform.cs FBox.cs all aded

---
+++ UAssetEditor/Unreal/Properties/Structs/Math/FIntVector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UAssetEditor.Classes;
+
+namespace UAssetEditor.Unreal.Properties.Structs.Math;
+
+public struct FIntVector : IUnrealType
+{
+    [UField]
+    public int X;
+    [UField]
+    public int Y;
+    [UField]
+    public int Z;
+}
