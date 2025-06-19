6064246f8afd2ec92c12f34d33c5ae9920f49f68
Dave <renegadesglitches@gmail.com>
2025-06-19 17:26:39 +0400

    FPlane.cs

---
+++ UAssetEditor/Unreal/Properties/Structs/Math/FPlane.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UAssetEditor.Classes;
+
+namespace UAssetEditor.Unreal.Properties.Structs.Math;
+
+
+public struct FPlane : IUnrealType
+{
+    [UField]
+    public FVector3 Vector;
+    [UField]
+    public float W;
+}
+public struct FPlane_LARGE_WORLD_COORDINATES : IUnrealType
+{
+    [UField]
+    public FVector3_LARGE_WORLD_COORDINATES Vector;
+    [UField]
+    public double W;
+}
