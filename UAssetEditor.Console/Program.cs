68c129eba37cfa7f0eff91df712c329e63488bca
Owen <47339578+oshock@users.noreply.github.com>
2025-08-10 02:58:25 -0500

    Some markup changes and SoftObjectProperty editing

--- UAssetEditor.Console/Program.cs
+++ UAssetEditor.Console/Program.cs
@@ -7,15 +7,17 @@ using UAssetEditor;
 using UAssetEditor.Compression;
 using UAssetEditor.Encryption.Aes;
 using UAssetEditor.Unreal.Misc;
+using UAssetEditor.Unreal.Properties;
+using UAssetEditor.Unreal.Properties.Types;
 
 // Construct USystem
 Oodle.Initialize("oo2core_9_win64.dll");
-var paks = AnsiConsole.Prompt(new TextPrompt<string>("What is the path to your paks folder?"));
+var paks = AnsiConsole.Prompt(new TextPrompt<string>("[blue]What is the path to your paks folder?[/]"));
 var uSystem = new UnrealFileSystem(paks);
 
 // Use GenxGames.gg for Aes Keys
 var useAesEndpoint = AnsiConsole.Prompt(
-    new TextPrompt<bool>("\nShould we use 'https://fortnitecentral.genxgames.gg/api/v1/aes' for the latest AES keys?")
+    new TextPrompt<bool>("[aqua]Should we use '[white]https://fortnitecentral.genxgames.gg/api/v1/aes[/]' for the latest AES keys?[/]")
         .AddChoice(true)
         .AddChoice(false)
         .DefaultValue(true)
@@ -52,7 +54,7 @@ var mountingTask = Task.Run(() =>
 await AnsiConsole.Progress()
     .StartAsync(async ctx =>
     {
-        var task = ctx.AddTask("[green]Mounting containers...[/]");
+        var task = ctx.AddTask("[aqua]Mounting containers...[/]");
         var totalFiles = Convert.ToDouble(uSystem.Files.Count(x => x.EndsWith(".utoc")));
         var last = 0.0;
         
@@ -68,7 +70,7 @@ await AnsiConsole.Progress()
     
 // Use GenxGames.gg for Mappings
 var useMappingsEndpoint = AnsiConsole.Prompt(
-    new TextPrompt<bool>("Should we use 'https://fortnitecentral.genxgames.gg/api/v1/mappings' for the latest Mappings?")
+    new TextPrompt<bool>("[aqua]Should we use '[white]https://fortnitecentral.genxgames.gg/api/v1/mappings[/]' for the latest Mappings?[/]")
         .AddChoice(true)
         .AddChoice(false)
         .DefaultValue(true)
@@ -87,18 +89,20 @@ if (useMappingsEndpoint)
 }
 else
 {
-    var mappingsPath = AnsiConsole.Prompt(new TextPrompt<string>("Enter the path to your mappings file (.usmap)")).Replace("\"", "").Replace("/", "\\").Trim();
+    AnsiConsole.MarkupLine("[blue]Enter the path to your mappings file[/] (.usmap)");
+    var mappingsPath = AnsiConsole.Prompt(new TextPrompt<string>("[aqua]>>>[/]")).Replace("\"", "").Replace("/", "\\").Trim();
     uSystem.LoadMappings(mappingsPath, "oo2core_9_win64.dll");
 }
 
 extractAsset:
-var assetPath = AnsiConsole.Prompt(new TextPrompt<string>("Enter a path to an asset (ex: 'FortniteGame/Content/Asset.uasset')")).Replace("\\", "/").Trim();
+AnsiConsole.MarkupLine("[blue]Enter a path to an asset [/](ex: 'FortniteGame/Content/Asset.uasset')");
+var assetPath = AnsiConsole.Prompt(new TextPrompt<string>(">>>")).Replace("\\", "/").Trim();
 AnsiConsole.MarkupLine($"[blue]Extracting '[white]{assetPath}[/]'...[/]");
 
 if (!uSystem.TryExtractAsset(assetPath, out var asset))
 {
-    AnsiConsole.MarkupLine($"[blue]Unable to extract '[white]{asset}[/]'[/]");
-    AnsiConsole.MarkupLine("Press any key to try another asset...");
+    AnsiConsole.MarkupLine($"[red]Unable to extract '[white]{asset}[/]'[/]");
+    AnsiConsole.MarkupLine("[red]Press any key to try another asset...[/]");
     Console.ReadKey();
     goto extractAsset;
 }
@@ -125,7 +129,10 @@ var exportName = AnsiConsole.Prompt(
         .AddChoices(asset.Exports.Select(x => x.Name)));
 var export = asset.Exports[exportName];
 
-var properties = export!.Properties.Select(x => Markup.Escape($"{x.Name} ({x.Data?.Type ?? "None"}): {x.Value}"));
+var properties = export!.Properties.Select(x => Markup.Escape($"{x.Name} ({x.Data?.Type ?? "None"})")).ToList();
+properties.Insert(0, "..");
+
+openProperty:
 var propertyName = AnsiConsole.Prompt(
     new SelectionPrompt<string>()
         .Title("[blue]Pick a property to modify[/]:")
@@ -133,6 +140,40 @@ var propertyName = AnsiConsole.Prompt(
         .MoreChoicesText("[grey](Move up and down to show more properties)[/]")
         .AddChoices(properties));
 
+if (propertyName == "..")
+    goto openExport;
+
 var property = export.Properties[properties.ToList().FindIndex(x => x == propertyName)];
-Console.ReadKey();
+PrintPropertyInfo(property);
+
+switch (property.Data?.Type)
+{
+    case "ArrayProperty":
+    {
+        var array = property.GetValue<ArrayProperty>();
+        foreach (var elm in array.Value)
+        {
+
+        }
+
+        break;
+    }
+    case "SoftObjectProperty":
+    {
+        var value = property.GetValue<SoftObjectProperty>();
+        AnsiConsole.MarkupLine($"[blue]\"[white]{value?.Value ?? "None"}[/]\"[/]");
+        AnsiConsole.MarkupLine("[blue]Enter a new value [white](ex: /Game/Path.Name)[/] or type '.' to exit...[/]");
+        var newValue = AnsiConsole.Prompt(new TextPrompt<string>("[aqua]>>>[/]"));
+        if (newValue != ".")
+            value!.Value = newValue;
         
+        goto openProperty;
+    }
+}
+
+Console.ReadKey();
+
+void PrintPropertyInfo(UProperty prop)
+{
+    AnsiConsole.MarkupLine($"[blue]{prop.Name} ([white]{prop.Data?.Type}[/])[/]");
+}
